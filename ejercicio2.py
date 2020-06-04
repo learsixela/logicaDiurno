57393fa264bd42f98080fdd3edeb64950aaedb3b
israelpalma <ipalm001@gmail.com>
2020-06-04 16:02:41 -0400

    Clase dia 04 06 2020

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+nombre = input("Ingresa tu nombre")
+print(nombre.upper())#pasar a mayuscula
+print(nombre.lower())#pasar a minuscula
+
+opcion= input("Tienes covid-19? [S/N] ")
+print(opcion)
+
+
---
+++ venv/operadores.py
@@ -0,0 +1,21 @@
+print("Operadors aritmeticos")
+
+#+, -, *, /, %, //, **
+
+suma = 2+3
+resta = 6-3
+multi= 2*5
+division= 7/3
+resto = 7%3 #resto de la division
+parteEntera= 7//3 #parte entera de la división
+elevado = 3**4 #numero elevado a otro numero
+
+#print
+print("La suma",suma)
+print(resta)
+print(multi)
+print(division)
+print(resto)
+print(parteEntera)
+print(elevado)
+
